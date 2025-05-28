edf0f5955f32f90608131d98f804cb98fa52b0cd
Lucky7 <mihaitibrea2@gmail.com>
2025-05-28 14:00:56 +0300

    footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TextService } from '../../services/text.service'; // adjust path if needed
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+
+interface FooterTexts {
+  brand: string;
+  subtitle: string;
+  links: {
+    terms: string;
+    privacy: string;
+    contact: string;
+  };
+  copyright: string;
+}
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
-
+  private textService = inject(TextService);
+  readonly t$: Observable<FooterTexts> = this.textService.section<FooterTexts>('footer');
   currentYear = new Date().getFullYear();
-
 }
