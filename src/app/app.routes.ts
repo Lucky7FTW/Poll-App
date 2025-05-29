9414a0aea6b6732058a39e62284544a6924f1cf3
GengiuRobert <fncrobert3@gmail.com>
2025-05-29 18:30:13 +0300

    contact page in footer created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { PollResultsComponent } from './components/poll-results/poll-results.com
 import { LoginComponent } from './components/login/login.component';
 import { PrivatePollsComponent } from './components/private-polls/private-polls.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ContactComponent } from './components/footer/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -25,5 +26,6 @@ export const routes: Routes = [
   { path: 'poll/:id/results', component: PollResultsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' },
 ];
